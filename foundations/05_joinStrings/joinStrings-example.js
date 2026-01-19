06ae4da0053b8c63cd430520385cd55139313de3
Dhruv Sunil Parmar <parmar.dhru@northeastern.edu>
2026-01-19 15:26:55 -0500

    finished foundations/05

--- foundations/05_joinStrings/joinStrings-example.js
+++ foundations/05_joinStrings/joinStrings-example.js
@@ -5,7 +5,7 @@ const lastName = "Stevenson";
 
 const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
 
-console.log(greeting);
+console.log(greeting); // answer: Hello! My name is Carlos Stevenson and I am 18 years old.
 
 module.exports = {
   greeting,
